d4541b009016c62fca28ad99f28b630736876b68
Juru01 <jurusophie@gmail.com>
2025-02-12 18:24:34 +0300

    added index.php wgich is the landing page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample Web app</title>
+</head>
+<body>
+    <div class="header">
+        <h1>
+            My first web app
+        </h1>
+    </div>
+</body>
+</html>
